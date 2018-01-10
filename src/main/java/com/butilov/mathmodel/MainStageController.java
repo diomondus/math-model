31f53560cdcf7d0cf6fae9e5037ca5b52497c4c5
Dmitry Butilov <acebutilov@gmail.com>
2018-01-10 23:23:20 +0300

    Добавляет тестовый график на сцену

--- src/main/java/com/butilov/mathmodel/MainStageController.java
+++ src/main/java/com/butilov/mathmodel/MainStageController.java
@@ -1,6 +1,9 @@
 package com.butilov.mathmodel;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +25,46 @@ public class MainStageController {
         testButton.setOnAction(e -> {
             eventTextField.appendText("hello button pressed \n");
         });
+
+        initChartData();
+    }
+
+    private void initChartData() {
+        xAxis.setLabel("Number of Month");
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        series.getData().add(new XYChart.Data(6, 36));
+        series.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+        lineChart.getData().add(series);
     }
 
     @FXML
     private Button testButton;
     @FXML
     private TextArea eventTextField;
+    @FXML
+    NumberAxis xAxis;
+    @FXML
+    NumberAxis yAxis;
+    @FXML
+    LineChart<Number, Number> lineChart;
 
     private FXTestBean mFXTestBean;
 
     public void onApplicationExit() {
-        // todo
+        // todo если понадобится
     }
 }
