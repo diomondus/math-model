eff06950e1fedffa1af33972fc541d47bb461e50
Dmitry Butilov <acebutilov@gmail.com>
2018-01-18 18:59:52 +0300

    Убирает импорт для конфигурациит Spring
    Импорт не требуется из-за того, что используется ComponentScan с указанным пэкаджем, в котором будет произведен поиск конфигураций спрингом и которые он подцепит автоматически, если указана аннотация конфигурации.

--- src/main/java/com/butilov/mathmodel/SpringConfiguration.java
+++ src/main/java/com/butilov/mathmodel/SpringConfiguration.java
@@ -3,7 +3,6 @@ package com.butilov.mathmodel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by Dmitry Butilov
@@ -11,12 +10,9 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @ComponentScan("com.butilov.mathmodel")
-@Import(FXConfiguration.class) // todo почему работает без этой строки?
-public class SpringConfiguration
-{
+public class SpringConfiguration {
     @Bean
-    public FXTestBean getBean()
-    {
+    public FXTestBean getBean() {
         return new FXTestBean();
     }
 }
