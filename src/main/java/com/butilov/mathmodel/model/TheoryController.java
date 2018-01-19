47843783884db7a2c644272c7bb8158209c43b1d
Dmitry Butilov <acebutilov@gmail.com>
2018-01-19 05:09:25 +0300

    Делает класс бином

--- src/main/java/com/butilov/mathmodel/model/TheoryController.java
+++ src/main/java/com/butilov/mathmodel/model/TheoryController.java
@@ -1,8 +1,11 @@
 package com.butilov.mathmodel.model;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Dmitry Butilov
  * on 15.01.18.
  */
+@Component
 public class TheoryController {
 }
