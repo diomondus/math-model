9d2024bd7ff6fba5e059936f746fe3e1cfae3354
Dmitry Butilov <acebutilov@gmail.com>
2018-01-21 21:48:36 +0300

    Добавляет локализацию и описание вкладки модели

--- src/main/java/com/butilov/mathmodel/controllers/AboutController.java
+++ src/main/java/com/butilov/mathmodel/controllers/AboutController.java
@@ -1,6 +1,12 @@
 package com.butilov.mathmodel.controllers;
 
+import com.butilov.mathmodel.localization.I18N;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 /**
  * Created by Dmitry Butilov
@@ -8,4 +14,27 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class AboutController {
+    @Autowired
+    void setI18N(I18N aI18N) {
+        mI18N = aI18N;
+    }
+
+    @FXML
+    public void initialize() {
+        aboutTheoryHeader.textProperty().bind(mI18N.createStringBinding("about.header.theory"));
+        aboutModelHeader.textProperty().bind(mI18N.createStringBinding("about.header.model"));
+        aboutTheoryTextArea.textProperty().bind(mI18N.createStringBinding("about.textarea.theory"));
+        aboutModelTextArea.textProperty().bind(mI18N.createStringBinding("about.textarea.model"));
+    }
+
+    @FXML
+    private Label aboutTheoryHeader;
+    @FXML
+    private Label aboutModelHeader;
+    @FXML
+    private JFXTextArea aboutTheoryTextArea;
+    @FXML
+    private JFXTextArea aboutModelTextArea;
+
+    private I18N mI18N;
 }
